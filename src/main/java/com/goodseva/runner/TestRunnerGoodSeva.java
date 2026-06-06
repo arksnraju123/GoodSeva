7d90d915e3f32d991e5e6b0dc460b2de21819b4b
arksnraju123 <arksnraju.alluri@gmail.com>
2026-06-06 19:04:24 +0530

    Completed Appointments module and added Zones module coverage

--- src/main/java/com/goodseva/runner/TestRunnerGoodSeva.java
+++ src/main/java/com/goodseva/runner/TestRunnerGoodSeva.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        tags = "@Appointments01",
+        tags = "@GoodsPlyRegression",
         glue = { "com.goodseva", "com.goodseva.hooks" },
         features = "src/test/resources/features",
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","rerun:target/rerun.txt" })
